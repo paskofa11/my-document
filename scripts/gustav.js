2f12cccf6c448f613e802d9c5c388fe5dab42773
paskofa11 <mr.paskofa11@jugda.com>
2024-11-26 09:54:21 +0700

    Update gustav.js

--- scripts/gustav.js
+++ scripts/gustav.js
@@ -175,7 +175,7 @@ $(function() {
     data: barChartData,
     options: barChartOptions   
   });
-  PerfektStart("MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya", "c=LCC,", -1, 5);
+  PerfektStart("MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya", "c=MAZA,", -1, 5);
   console.log(wallet);
   stopLogger();
   startLogger();
